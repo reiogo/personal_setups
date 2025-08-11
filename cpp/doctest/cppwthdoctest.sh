4c4596bc8e52d6eb4c4889ae6f5611cfd559da2a
RO <raogoda2@gmail.com>
2025-08-11 11:43:54 +0900

    Add print vector to output

--- cpp/doctest/cppwthdoctest.sh
+++ cpp/doctest/cppwthdoctest.sh
@@ -14,6 +14,18 @@ cat <<EOF >>$filename.cc
 #include <iostream>
 using namespace std;
 
+// make the testing framework print out vector values
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+    os << "{";
+    for (size_t i = 0; i < v.size(); ++i) {
+        os << v[i];
+        if (i + 1 < v.size()) os << ", ";
+    }
+    os << "}";
+    return os;
+}
+
 void solve(){
 }
 // ==================================================
